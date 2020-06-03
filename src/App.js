787a4cdcbc4f46be83bf92eafbed09391073fbe9
Alaa Yahia <alaay873@live.com>
2020-06-03 21:58:14 +0200

    Mockup html & css

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-
-  return (
-    <div className="App">
-      
-    </div>
+  
+    return (
+      <div className="game">
+        <div className="help">
+          Does the meaning of the top word match the ink
+          color of the bottom word?
+        </div>
+        <div className="body">
+          <div className="game-status status-playing" />
+          <div className="meaning">*Meaning*</div>
+          <div className="ink">*Ink*</div>
+          <div className="buttons">
+            <button>YES</button>
+            <button>NO</button>
+          </div>
+        </div>
+      </div>
   );
 }
 
