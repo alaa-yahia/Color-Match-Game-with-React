4a77e1721abdd28e5bcd8c1ffb4c20d80e7d2405
Alaa Yahia <alaay873@live.com>
2021-09-11 11:12:27 +0200

    Updates

---
+++ precache-manifest.1a71681b577bbf87ca77fdc7d1d87b89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b532b32e292c17ab979d84daf6d3cf07",
+    "url": "/Color-Match-Game-with-React/index.html"
+  },
+  {
+    "revision": "539abacc1fed0fd400db",
+    "url": "/Color-Match-Game-with-React/static/css/main.80e32c55.chunk.css"
+  },
+  {
+    "revision": "1c907d72f5ea20ab4365",
+    "url": "/Color-Match-Game-with-React/static/js/2.92a8770e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Color-Match-Game-with-React/static/js/2.92a8770e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "539abacc1fed0fd400db",
+    "url": "/Color-Match-Game-with-React/static/js/main.ba2ca237.chunk.js"
+  },
+  {
+    "revision": "fbd3288d217ded7771c2",
+    "url": "/Color-Match-Game-with-React/static/js/runtime-main.a150fb7a.js"
+  }
+]);
